7a62f4fd63abdcbb374286a588c5fa279fdf6ac7
Emily Dittmar <Dittmare@gmail.com>
2020-07-15 19:02:49 -0400

    Create AlignBowtie.sh

---
+++ SNP-calling/TruthSNPs/AlignBowtie.sh
@@ -0,0 +1,29 @@
+#PBS -S /bin/bash
+#PBS -q batch
+#PBS -N bowtie_Align
+#PBS -l mem=48gb,nodes=1:ppn=6,walltime=06:00:00
+#PBS -m abe
+#PBS -M dittmare@gmail.com
+
+set -e
+set -o pipefail
+
+module load Bowtie2/2.3.5.1-GCC-8.3.0
+
+# This script does a quick alignment as a check for the reference genome
+# Following what Li did for morex v1: https://github.com/lilei1/9k_BOPA_SNP/blob/master/script/commandlines
+
+# User provided input arguments
+FASTA_FILE=/scratch/eld72413/SNParray/ContextualSeqs.fasta
+DB_NAME=/scratch/eld72413/Ha412HOv2.0/Ha412HOv2.0-20181130
+OUT_PREFIX=Bachlava_Contextual_HA412v2_bowtie2
+OUT_DIR=/scratch/eld72413/SNParray/AllSNPs
+
+# Check if our dir exists, if not make it
+#mkdir -p ${OUT_DIR}
+
+# Go into reference dir
+cd ${OUT_DIR}
+
+# Align with bowtie2
+bowtie2 -f ${FASTA_FILE} -x ${DB_NAME} -S ${OUT_PREFIX}.sam
