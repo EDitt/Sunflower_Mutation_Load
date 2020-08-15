80444d16ecb16e401b7cf465e2863fd81344a6ae
Emily Dittmar <edittmar@uga.edu>
2020-08-15 02:05:41 -0400

    Parallelize concatenate script

--- SNP-calling/DataProcessing/Concatenate.sh
+++ SNP-calling/DataProcessing/Concatenate.sh
@@ -4,6 +4,7 @@
 #PBS -l nodes=1:ppn=6
 #PBS -l walltime=80:00:00
 #PBS -l mem=20gb
+#PBS -t 1-50
 
 #PBS -M dittmare@gmail.com
 #PBS -m abe
@@ -11,19 +12,17 @@
 INPUTDIR=/scratch/eld72413/SAM_seq/results2/Group4/RawSeqs
 OUTPUTDIR=/scratch/eld72413/SAM_seq/results2/Group4/MergedSeqs
 
-cd $INPUTDIR
+DIR=$(find $INPUTDIR -name "PPN*" | sed -n ${PBS_ARRAYID}p)
 
-for d in PPN*/; do
-	if [[ -d "$d" ]]; then
-		name=${d%%/}""
-		for f1 in `ls "$d"*R1.fastq.gz`; do
-			if [[ -f "$f1" ]]; then
-				f2=${f1%%1.fastq.gz}"2.fastq.gz"
-				echo "Concatenating $name R1"
-				zcat $f1 >> ${OUTPUTDIR}/"$name"_R1.fastq.gz
-				echo "Concatenating $name R2"
-				zcat $f2 >> ${OUTPUTDIR}/"$name"_R2.fastq.gz
-			fi
-		done
-	fi
-done
+if [[ -d "$DIR" ]]; then
+	name=$(basename ${DIR})
+	for f1 in `ls "$DIR"/*R1.fastq.gz`; do
+		if [[ -f "$f1" ]]; then
+			f2=${f1%%1.fastq.gz}"2.fastq.gz"
+			echo "Concatenating $name R1"
+			zcat $f1 >> ${OUTPUTDIR}/"$name"_R1.fastq.gz
+			echo "Concatenating $name R2"
+			zcat $f2 >> ${OUTPUTDIR}/"$name"_R2.fastq.gz
+		fi
+	done
+fi
