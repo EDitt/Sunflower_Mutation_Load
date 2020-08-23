853a08bfba44ed601b7580a4c88de2318d4b7b14
Emily Dittmar <Dittmare@gmail.com>
2020-08-23 17:03:09 -0400

    Vep script

--- BAD_Mutations/VeP.sh
+++ BAD_Mutations/VeP.sh
@@ -1,43 +1,37 @@
-#!/bin/bash -l
+#PBS -S /bin/bash
 #PBS -q batch
 #PBS -N VEP
 #PBS -l nodes=1:ppn=1
 #PBS -l walltime=18:00:00
-#PBS -l mem=4gb
+#PBS -l mem=22gb
 #PBS -m abe
 #PBS -M dittmare@gmail.com
-
 ​
-module load HTSlib/1.9-foss-2018b
+#module load HTSlib/1.9-foss-2018b
 #module load perl/modules.centos7.5.26.1
-module load BCFtools/1.6-foss-2016b
+#module load BCFtools/1.6-foss-2016b
 
-#     The original VCF
-VCF=.vcf
-Compressed_VCF=.vcf.gz
+#     The VCF
+Compressed_VCF=/scratch/eld72413/NSFproj/PublishedSNPs/UBC_Dataset/Annuus.tranche90.snp.fullsam.90.bi.remappedHa412HO_reheader.vcf.gz
 ​
-#    Compress the VCF file
-#bgzip --index ${VCF}
-#bcftools view ${VCF} --output-file ${Compressed_VCF} -output-type z
-​
-#while [ ! -f ${Compressed_VCF} ]; do sleep 5; done
-​
-#bcftools index --force ${Compressed_VCF}
-​
+OUTPUTDIR=/scratch/eld72413/NSFproj/VEP
+
 #    Variant sets should be either 'deletions', 'insertions', or 'snps'
 VARIANT_SET=all
 ​
-module avail VEP/95.0-foss-2018b-Perl-5.28.0
-cd 
-​
-/home/morrellp/shared/Software/ensembl-vep-release-97.3/vep \
+module load VEP/95.0-foss-2018b-Perl-5.28.0
+# cd /usr/local/singularity-images/
+# singularity exec ./ensembl-vep.simg vep \
+
+vep \
     -i ${Compressed_VCF} \
-    --gff /scratch/eld72413/Ha412HOv2.0/Ha412HOv2.0-20181130.gff3 \
+    --gff /scratch/eld72413/Ha412HOv2.0/Ha412HOv2.0.gff3.gz \
     --fasta  /scratch/eld72413/Ha412HOv2.0/Ha412HOv2.0-20181130.fasta \
-    --species  \
+    --species  helianthus_annuus \
     --total_length \
     --check_svs \
     --verbose \
     --format vcf \
-    --warning_file Morex_Mutants_${VARIANT_SET}.txt \
-    -o Morex_Mutants_${VARIANT_SET}.txt
+    --warning_file ${OUTPUTDIR}/fullsam_remappedHa412HO_${VARIANT_SET}_WARN.txt \
+    -o ${OUTPUTDIR}/fullsam_remappedHa412HO_${VARIANT_SET}.txt
+
