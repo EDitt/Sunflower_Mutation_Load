2c7cbf39181c757b30c9cda6b936fdffd4227fef
Emily Dittmar <edittmar@uga.edu>
2020-08-29 20:22:34 -0400

    Fix Cat_SRA

--- Outgroups/Cat_SRA.sh
+++ Outgroups/Cat_SRA.sh
@@ -13,16 +13,15 @@ LIST=/home/eld72413/DelMut/Sunflower_Mutation_Load/Outgroups/WildAnnuusOnePerPop
 INPUTDIR=/scratch/eld72413/NSFproj/ancestralseqs/Annuus/Raw
 OUTPUTDIR=/scratch/eld72413/NSFproj/ancestralseqs/Annuus/Raw/Merged
 
-Sample=$(sed -n ${PBS_ARRAYID}p | awk '{print $2}')
+Sample=$(sed -n ${PBS_ARRAYID}p $LIST | awk '{print $2}')
 
 NumFiles=$(find -maxdepth 1 -name "${Sample}*1.fastq" | wc -l)
 
-if [[ "$NumFiles" -eq 0 ]]
-then
-echo "No $Sample Files in $INPUTDIR"
+if [[ "$NumFiles" -eq 0 ]]: then
+	echo "No $Sample Files in $INPUTDIR"
 else
-echo "Concatenating $NumFiles Forward $Sample"
-cat `ls ${INPUTDIR}/${Sample}*1.fastq` > ${OUTPUTDIR}/"$Sample"_R1.fastq
-echo "Concatenating $NumFiles Reverse $Sample"
-cat `ls ${INPUTDIR}/${Sample}*2.fastq` > ${OUTPUTDIR}/"$Sample"_R2.fastq
+	echo "Concatenating $NumFiles Forward $Sample"
+	cat `ls ${INPUTDIR}/${Sample}*1.fastq` > ${OUTPUTDIR}/"$Sample"_R1.fastq
+	echo "Concatenating $NumFiles Reverse $Sample"
+	cat `ls ${INPUTDIR}/${Sample}*2.fastq` > ${OUTPUTDIR}/"$Sample"_R2.fastq
 fi
