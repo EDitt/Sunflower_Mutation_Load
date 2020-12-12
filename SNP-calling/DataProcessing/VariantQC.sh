2a2153114fda53309f77a1b0e4e231f688c63000
Emily Dittmar <edittmar@uga.edu>
2020-12-12 01:46:18 -0500

    slurm job, report filter status

--- SNP-calling/DataProcessing/VariantQC.sh
+++ SNP-calling/DataProcessing/VariantQC.sh
@@ -1,34 +1,32 @@
-#PBS -S /bin/bash
-#PBS -q batch
-#PBS -N VariantQC
-#PBS -l nodes=1:ppn=6
-#PBS -l walltime=22:00:00
-#PBS -l mem=22gb
+#!/bin/bash
 
-#PBS -M dittmare@gmail.com
-#PBS -m abe
+#SBATCH --job-name=Variant_Table
+#SBATCH --partition=batch
 
-module load GATK/4.1.2.0-GCCcore-8.2.0-Java-1.8
-GATK_JAR=/usr/local/apps/eb/GATK/4.1.2.0-GCCcore-8.2.0-Java-1.8/gatk
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=22G
+#SBATCH --time=12:00:00
+#SBATCH --export=None 
+#SBATCH --mail-user=dittmare@gmail.com
+#SBATCH --mail-type=BEGIN,END,FAIL
 
-module load VCFtools/0.1.15-foss-2016b-Perl-5.24.1
+module load GATK/4.1.3.0-GCCcore-8.3.0-Java-1.8
+GATK_JAR=/apps/eb/GATK/4.1.3.0-GCCcore-8.3.0-Java-1.8/gatk
 
-INPUT_VCF=/scratch/eld72413/SAM_seq/results2/VCF_results_new/Create_HC_Subset/New2/Filter1_102120/Sunflower_SAM_SNP_Calling_snps.filtered.vcf
-OUTPUT_DIR=/scratch/eld72413/SAM_seq/results2/VCF_results_new/Create_HC_Subset/New2/Filter1_102120/QC
+INPUT_VCF=/scratch/eld72413/SAM_seq/results2/VCF_results_new/Create_HC_Subset/New2/Variant_Recalibrator/Sunflower_SAM_SNP_Calling_snps.recalibrated.vcf.gz
+OUTPUT_DIR=/scratch/eld72413/SAM_seq/results2/VCF_results_new/Create_HC_Subset/New2/Variant_Recalibrator
 INTERVALS=/scratch/eld72413/SAM_seq/results2/VCF_results_new/Create_HC_Subset/New2/Create_HC_Subset/Intermediates/Genome_Random_Intervals.bed
 
-
-num_sites=$(grep -v "#" "${INPUT_VCF}" | wc -l)
-echo "Number of sites is ${num_sites}"
-
-cd $OUTPUT_DIR
-vcftools --TsTv-by-qual ${INPUT_VCF}
 
 echo "Using GATK's variants to table function"
 gatk VariantsToTable \
      -V "${INPUT_VCF}" \
      -L "${INTERVALS}" \
-     -F CHROM -F POS -F TYPE -F QUAL -F QD -F DP -F MQ -F MQRankSum -F FS -F ReadPosRankSum -F SOR -F TRANSITION -F HET \
-     -GF GQ -GF DP \
-     -O "${OUTPUT_DIR}/FilteredVariants.table"
+     -F CHROM -F POS -F TYPE -F QUAL -F FILTER -F QD -F DP -F MQ -F MQRankSum -F FS -F ReadPosRankSum -F SOR -F TRANSITION -F HET \
+     -O "${OUTPUT_DIR}/Variants.table" \
+     --show-filtered
 echo "done"
+
+#    -GF GQ -GF DP \
