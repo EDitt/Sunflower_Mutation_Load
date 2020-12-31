ca3ae294602338eb16c643d081cc89cf5b0f8476
Emily Dittmar <Dittmare@gmail.com>
2020-12-31 18:15:12 -0500

    select variants above qual=40

---
+++ SNP-calling/DataProcessing/gatk_SelectVariantsQUAL.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#SBATCH --job-name=Select_VariantsQUAL
+#SBATCH --partition=batch
+
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=22G
+#SBATCH --time=36:00:00
+#SBATCH --export=None 
+#SBATCH --mail-user=dittmare@gmail.com
+#SBATCH --mail-type=BEGIN,END,FAIL
+
+module load GATK/4.1.3.0-GCCcore-8.3.0-Java-1.8
+GATK_JAR=/apps/eb/GATK/4.1.3.0-GCCcore-8.3.0-Java-1.8/gatk
+
+GEN_FASTA="/scratch/eld72413/Ha412HOv2.0/Ha412HOv2.0-20181130.fasta"
+INPUT_VCF="/scratch/eld72413/SAM_seq/results2/VCF_results_new/Create_HC_Subset/New2/Filter2_122828/Sunflower_SAM_SNP_Calling_GenoFieldFiltered.vcf"
+OUTPUT_DIR="/scratch/eld72413/SAM_seq/results2/VCF_results_new/Create_HC_Subset/New2/Filter3_123120"
+TEMP_DIR="/scratch/eld72413/Tmp"
+
+# filtering: 
+# QUAL > 40
+
+gatk --java-options "-Xmx22g" SelectVariants \
+-R ${GEN_FASTA} \
+-V ${INPUT_VCF} \
+-O ${OUTPUT_DIR}/Sunflower_SAM_SNP_Calling_QUALFiltered.vcf \
+--selectExpressions "QUAL > 40.0" \
+--tmp-dir ${TEMP_DIR}
