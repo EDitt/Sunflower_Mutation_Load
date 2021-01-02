50986cf6b2076c7de1208b285b30d9e608aa9a5e
Emily Dittmar <edittmar@uga.edu>
2021-01-01 22:53:11 -0500

    cannot invalidate previous genotype filters
    using earlier vcf before genotype quality and depth filters applied

--- SNP-calling/Variant_Filtering/Het_filter.sh
+++ SNP-calling/Variant_Filtering/Het_filter.sh
@@ -16,20 +16,19 @@ module load GATK/4.1.3.0-GCCcore-8.3.0-Java-1.8
 GATK_JAR=/apps/eb/GATK/4.1.3.0-GCCcore-8.3.0-Java-1.8/gatk
 
 REF_FASTA="/scratch/eld72413/Ha412HOv2.0/Ha412HOv2.0-20181130.fasta"
-INPUT_VCF="/scratch/eld72413/SAM_seq/results2/VCF_results_new/Create_HC_Subset/New2/Filter3_123120/Sunflower_SAM_SNP_Calling_QUALFiltered.vcf"
+INPUT_VCF="/scratch/eld72413/SAM_seq/results2/VCF_results_new/Create_HC_Subset/New2/Filter1_102120/Sunflower_SAM_SNP_Calling_snps.filtered.vcf"
 OUTPUT_DIR="/scratch/eld72413/SAM_seq/results2/VCF_results_new/Create_HC_Subset/New2/Het_Filter_010121"
 TEMP_DIR="/scratch/eld72413/Tmp"
 
 
-echo "Marking heterozygote genotypes and removing previous filters..."
+echo "Marking heterozygote genotypes..."
 gatk --java-options "-Xmx22g" VariantFiltration \
 	-R ${REF_FASTA} \
 	-V ${INPUT_VCF} \
 	-O ${OUTPUT_DIR}/Sunflower_SAM_SNP_Calling_HetField.vcf \
 	--genotype-filter-name "isHet" --genotype-filter-expression "isHet == 1"  \
-	--invalidate-previous-filters true \
 	--tmp-dir ${TEMP_DIR}
-echo "Finished marking heterozygote genotypes and removing previous filters"
+echo "Finished marking heterozygote genotypes"
 
 echo "Selecting sites with no more than 20% heterozygotes"
 # filtering: 
