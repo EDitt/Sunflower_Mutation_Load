6a66374aefde1bb055c061f6fe1d646019349544
Emily Dittmar <edittmar@uga.edu>
2021-01-19 17:05:29 -0500

    concordance for UBC SNP set

--- SNP-calling/DataProcessing/gatk_SelectConcordant.sh
+++ SNP-calling/DataProcessing/gatk_SelectConcordant.sh
@@ -16,9 +16,9 @@ module load GATK/4.1.3.0-GCCcore-8.3.0-Java-1.8
 GATK_JAR=/apps/eb/GATK/4.1.3.0-GCCcore-8.3.0-Java-1.8/gatk
 
 GEN_FASTA="/scratch/eld72413/Ha412HOv2.0/Ha412HOv2.0-20181130.fasta"
-INPUT_VCF="/scratch/eld72413/SAM_seq/results2/VCF_results_new/Create_HC_Subset/New2/Variant_Recalibrator/Sunflower_SAM_SNP_Calling_raw_variants_snps.vcf"
+INPUT_VCF="/scratch/eld72413/NSFproj/PublishedSNPs/UBC_Dataset_Raw/SAM_lines/UBC_Dataset_SAMlines.recode.vcf"
 TRUTH_VCF="/scratch/eld72413/SNParray/FinalFiles/MapUniqueSNP_idt90_rename_rmContigs_sorted.vcf"
-OUTPUT_DIR="/scratch/eld72413/SAM_seq/results2/VCF_results_new/Create_HC_Subset/New2/Filter6_011221/Concordance"
+OUTPUT_DIR="/scratch/eld72413/NSFproj/PublishedSNPs/UBC_Dataset_Raw/SAM_lines/Consensus"
 TEMP_DIR="/scratch/eld72413/Tmp"
 
 # this will output variants that are also in the truth set. Then I can see what their annotation values are to see if my filtering was too stringent
@@ -26,6 +26,6 @@ TEMP_DIR="/scratch/eld72413/Tmp"
 gatk --java-options "-Xmx22g" SelectVariants \
 -R ${GEN_FASTA} \
 -V ${INPUT_VCF} \
--O ${OUTPUT_DIR}/Sunflower_SAM_SNP_Calling_TruthConcordant.vcf \
+-O ${OUTPUT_DIR}/UBC_SAM_TruthConcordant.vcf \
 --concordance ${TRUTH_VCF} \
 --tmp-dir ${TEMP_DIR}
