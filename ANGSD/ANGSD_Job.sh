b3b9ff32d78a2e242ad985a1fc21b12ea22d2242
Emily Dittmar <Dittmare@gmail.com>
2021-01-20 15:43:56 -0500

    more memory?

--- ANGSD/ANGSD_Job.sh
+++ ANGSD/ANGSD_Job.sh
@@ -5,8 +5,8 @@
 
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=8
-#SBATCH --mem=22G
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=50G
 #SBATCH --time=36:00:00
 #SBATCH --export=None 
 #SBATCH --mail-user=dittmare@gmail.com
