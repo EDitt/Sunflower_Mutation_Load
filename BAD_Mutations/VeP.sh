a06530051bcb34468a2847dc5846589079848572
Emily Dittmar <Dittmare@gmail.com>
2021-01-20 16:24:45 -0500

    run VeP on normalized, biallelic vcf

--- BAD_Mutations/VeP.sh
+++ BAD_Mutations/VeP.sh
@@ -14,9 +14,9 @@
 
 
 #Compressed_VCF=/scratch/eld72413/NSFproj/PublishedSNPs/Edited/fullsam.90.remappedHa412HO_norm_biallelic.vcf.gz
-VCF="/scratch/eld72413/SAM_seq/results2/VCF_results_new/Create_HC_Subset/New2/Filter6_011221/Sunflower_SAM_SNP_Calling_Final_Filtered.vcf"
-OUTPUTDIR="/scratch/eld72413/SAM_seq/results2/VCF_results_new/Create_HC_Subset/New2/Filter6_011221"
-OUTPUTPREFIX=SAM_SNP_Final_FullSet
+VCF="/scratch/eld72413/SAM_seq/results2/VCF_results_new/Create_HC_Subset/New2/Filter6_011221/Biallelic/Sunflower_SAM_SNP_Calling_BIALLELIC_norm.vcf.gz"
+OUTPUTDIR="/scratch/eld72413/SAM_seq/results2/VCF_results_new/Create_HC_Subset/New2/Filter6_011221/Biallelic/VeP"
+OUTPUTPREFIX=SAM_SNP_Final_BiallelicNorm
 
 #    Variant sets should be either 'deletions', 'insertions', or 'snps'
 VARIANT_SET=all
