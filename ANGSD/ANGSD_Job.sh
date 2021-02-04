216f5fe98adc3eb7ac8b925d07c619164527ac1c
Emily Dittmar <Dittmare@gmail.com>
2021-02-04 13:18:42 -0500

    set temporary directory

--- ANGSD/ANGSD_Job.sh
+++ ANGSD/ANGSD_Job.sh
@@ -6,12 +6,11 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=8
-#SBATCH --mem=50G
+#SBATCH --mem=22G
 #SBATCH --time=36:00:00
 #SBATCH --export=None 
 #SBATCH --mail-user=dittmare@gmail.com
 #SBATCH --mail-type=BEGIN,END,FAIL
-#SBATCH --tmp=10g
 
 set -o pipefail
 
@@ -19,6 +18,8 @@ module load GSL/2.6-iccifort-2019.5.281
 #module load SAMtools/1.10-GCC-8.3.0
 #module load gnuplot/5.2.2-foss-2018a
 #module load R/3.6.1-foss-2018a-X11-20180131-GACRC
+
+TMPDIR="/scratch/eld72413/Tmp"
 
 cd /home/eld72413/ANGSD_DEV/new3_afterMaintenance/angsd-wrapper
 
