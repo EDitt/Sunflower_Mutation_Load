2868654c8b378ee99f652d40d012066251279bec
Emily Dittmar <Dittmare@gmail.com>
2021-02-04 17:49:19 -0500

    increase memory

--- ANGSD/ANGSD_Job.sh
+++ ANGSD/ANGSD_Job.sh
@@ -6,7 +6,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=8
-#SBATCH --mem=22G
+#SBATCH --mem=100G
 #SBATCH --time=36:00:00
 #SBATCH --export=None 
 #SBATCH --mail-user=dittmare@gmail.com
