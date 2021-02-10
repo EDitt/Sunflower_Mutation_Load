9cfc16c8cb9fa650a2a9818a27daeee2792b3034
Emily Dittmar <edittmar@uga.edu>
2021-02-10 17:05:55 -0500

    update job configuration to look like Sam's

--- ANGSD/ANGSD_Job.sh
+++ ANGSD/ANGSD_Job.sh
@@ -1,28 +1,32 @@
-#!/bin/bash
+#!/bin/bash -l
 
 #SBATCH --job-name=ANGSD
-#SBATCH --partition=batch
 
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=8
-#SBATCH --mem=50G
-#SBATCH --time=36:00:00
+#SBATCH --ntasks=16
+#SBATCH --mem=22G
+#SBATCH --time=6:00:00
 #SBATCH --export=None 
 #SBATCH --mail-user=dittmare@gmail.com
 #SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH -o %j.out
+#SBATCH -e %j.err
+#SBATCH --chdir=/scratch/eld72413/Tmp
 
 set -o pipefail
 
 module load GSL/2.6-iccifort-2019.5.281
+module load SAMtools/1.10-iccifort-2019.5.281
+module load gnuplot/5.2.8-GCCcore-8.3.0
+
 #module load SAMtools/1.10-GCC-8.3.0
 #module load gnuplot/5.2.2-foss-2018a
 #module load R/3.6.1-foss-2018a-X11-20180131-GACRC
 
 SLURM_TMPDIR="/scratch/eld72413/Tmp"
 
-cd /home/eld72413/ANGSD_DEV/new3_afterMaintenance/angsd-wrapper
+cd /home/eld72413/ANGSD_DEV/new4_recompileREDO/angsd-wrapper
 
-#./angsd-wrapper ${WRAPPER} ${CONFIG}
+./angsd-wrapper ${WRAPPER} ${CONFIG}
 
-./angsd-wrapper Inbreeding /scratch/eld72413/SAM_seq/ANGSD/Configuration_Files/Inbreeding_Coefficients_Config
+#./angsd-wrapper Inbreeding /scratch/eld72413/SAM_seq/ANGSD/Configuration_Files/Inbreeding_Coefficients_Config
