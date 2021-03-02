cb585b77a62d31112e8718dd504f76aec5d893e9
Emily Dittmar <Dittmare@gmail.com>
2021-03-02 14:23:24 -0500

    add intermediate step, check for existing files so I can rerun if needed

--- SNP-calling/Variant_Filtering/Filter_AllSteps.sh
+++ SNP-calling/Variant_Filtering/Filter_AllSteps.sh
@@ -21,8 +21,8 @@ GATK_JAR=/apps/eb/GATK/4.1.3.0-GCCcore-8.3.0-Java-1.8/gatk
 module load BCFtools/1.10.2-GCC-8.3.0
 
 GEN_FASTA="/scratch/eld72413/Ha412HOv2.0/Ha412HOv2.0-20181130.fasta"
-OUTPUT_DIR="/scratch/eld72413/SAM_seq/results2/VCF_results_new/Create_HC_Subset/New2/VarFilter_All"
-#OUTPUT_DIR="/scratch/eld72413/SAM_seq/results2/VCF_results_new/Create_HC_Subset/New2/VarFilter_All/Test"
+#OUTPUT_DIR="/scratch/eld72413/SAM_seq/results2/VCF_results_new/Create_HC_Subset/New2/VarFilter_All"
+OUTPUT_DIR="/scratch/eld72413/SAM_seq/results2/VCF_results_new/Create_HC_Subset/New2/VarFilter_All/Test"
 TEMP_DIR="/scratch/eld72413/Tmp"
 OUT_PREFIX="Sunflower_SAM_SNP"
 
@@ -38,91 +38,115 @@ mkdir -p ${OUTPUT_DIR}/Intermediates
 
 # Step 1: Removing sites that failed variant recalibrator
 
-echo "Selecting Pass Sites from Variant Recalibrator"
+if [[ -f "${OUTPUT_DIR}/Intermediates/${OUT_PREFIX}_recalibrated_pass_sites.vcf" ]]; then
+	echo "Recalibrated pass sites already selected, proceeding to step 2"
+else
+	echo "Selecting Pass Sites from Variant Recalibrator"
+	gatk SelectVariants \
+		-R ${GEN_FASTA} \
+		-V ${INPUT_VCF} \
+		--exclude-filtered true \
+		--create-output-variant-index true \
+		--select-type-to-include SNP \
+		--exclude-non-variants true \
+		--tmp-dir ${TEMP_DIR} \
+		-O ${OUTPUT_DIR}/Intermediates/${OUT_PREFIX}_recalibrated_pass_sites.vcf
 
-gatk SelectVariants \
+	num_sites1=$(grep -v "#" "${OUTPUT_DIR}/Intermediates/${OUT_PREFIX}_recalibrated_pass_sites.vcf" | wc -l)
+
+	echo "After filtering out sites that failed variant recalibrator, there are ${num_sites1} sites left"
+fi
+
+# Step 2: Removing low quality genotypes (min/max DP and min GQ) and sites with more than 0.2 missing genotypes
+
+if [[ -f "${OUTPUT_DIR}/Intermediates/${OUT_PREFIX}_GenoField.vcf.idx" ]]; then
+	echo "Filter labels already added to low quality genotypes"
+else
+	echo "Adding Filter labels to low quality genotypes. Cut-offs are as follows: MinGQ is ${GQ}; MinDP is ${MinDP}; MaxDP is ${MaxDP}"
+	gatk --java-options "-Xmx22g" VariantFiltration \
+		-R ${GEN_FASTA} \
+		-V ${OUTPUT_DIR}/Intermediates/${OUT_PREFIX}_recalibrated_pass_sites.vcf \
+		-O ${OUTPUT_DIR}/Intermediates/${OUT_PREFIX}_GenoField.vcf \
+		--genotype-filter-name "lowGQ" --genotype-filter-expression "GQ < ${GQ}"  \
+		--genotype-filter-name "highDP" --genotype-filter-expression "DP > ${MaxDP}" \
+		--genotype-filter-name "lowDP" --genotype-filter-expression "DP < ${MinDP}" \
+		--tmp-dir ${TEMP_DIR}
+
+	echo "Done adding filter labels to low quality genotypes"
+fi
+
+if [[ -f ${OUTPUT_DIR}/Intermediates/${OUT_PREFIX}_GenoFieldFiltered.vcf.idx ]]; then
+	echo "Filtered genotypes already set to no call, proceeding to step 3"
+else
+	echo "Removing sites with too much missing data, then setting filtered genotypes to no call"
+	gatk --java-options "-Xmx22g" SelectVariants \
 	-R ${GEN_FASTA} \
-	-V ${INPUT_VCF} \
-	--exclude-filtered true \
-	--create-output-variant-index true \
-	--select-type-to-include SNP \
+	-V ${OUTPUT_DIR}/Intermediates/${OUT_PREFIX}_GenoField.vcf \
+	-O ${OUTPUT_DIR}/Intermediates/${OUT_PREFIX}_GenoFieldFiltered.vcf \
+	--set-filtered-gt-to-nocall true \
+	--max-fraction-filtered-genotypes 0.2 \
+	--max-nocall-fraction 0.2 \
 	--exclude-non-variants true \
-	--tmp-dir ${TEMP_DIR} \
-	-O ${OUTPUT_DIR}/Intermediates/${OUT_PREFIX}_recalibrated_pass_sites.vcf
-
-num_sites1=$(grep -v "#" "${OUTPUT_DIR}/Intermediates/${OUT_PREFIX}_recalibrated_pass_sites.vcf" | wc -l)
-
-echo "After filtering out sites that failed variant recalibrator, there are ${num_sites1} sites left"
-
-# Step 2: Removing low quality genotypes (min/max DP and min GQ) and sites with more than 0.2 low quality genotypes
-
-echo "Adding Filter labels to low quality genotypes. Cut-offs are as follows: MinGQ is ${GQ}; MinDP is ${MinDP}; MaxDP is ${MaxDP}"
-
-gatk --java-options "-Xmx22g" VariantFiltration \
-	-R ${GEN_FASTA} \
-	-V ${OUTPUT_DIR}/Intermediates/${OUT_PREFIX}_recalibrated_pass_sites.vcf \
-	-O ${OUTPUT_DIR}/Intermediates/${OUT_PREFIX}_GenoField.vcf \
-	--genotype-filter-name "lowGQ" --genotype-filter-expression "GQ < ${GQ}"  \
-	--genotype-filter-name "highDP" --genotype-filter-expression "DP > ${MaxDP}" \
-	--genotype-filter-name "lowDP" --genotype-filter-expression "DP < ${MinDP}" \
+	--remove-unused-alternates true \
 	--tmp-dir ${TEMP_DIR}
 
-echo "Done adding filter labels to low quality genotypes. Removing sites with more than 0.2 low quality or missing genotypes"
+	num_sites2=$(grep -v "#" "${OUTPUT_DIR}/Intermediates/${OUT_PREFIX}_GenoFieldFiltered.vcf" | wc -l)
+	echo "After filtering out sites with too much missing variants, there are ${num_sites2} sites left"
+fi
 
-gatk --java-options "-Xmx22g" SelectVariants \
--R ${GEN_FASTA} \
--V ${OUTPUT_DIR}/Intermediates/${OUT_PREFIX}_GenoField.vcf \
--O ${OUTPUT_DIR}/Intermediates/${OUT_PREFIX}_GenoFieldFiltered.vcf \
---set-filtered-gt-to-nocall true \
---max-fraction-filtered-genotypes 0.2 \
---max-nocall-fraction 0.2 \
---exclude-non-variants true \
---remove-unused-alternates true \
---tmp-dir ${TEMP_DIR}
+### NOTE: It was discovered that the order of filtering by gatk in the previous step was that it removed nocall-fraction genotypes *before* it set filtered genotypes to no-call,
+# necessitating an intermediate step here to filter out sites with too many filtered genotypes - used bcftools (see bcftools_missingfilter.sh)
+# Step 3 below was added later when this was discovered:
 
-num_sites2=$(grep -v "#" "${OUTPUT_DIR}/Intermediates/${OUT_PREFIX}_GenoFieldFiltered.vcf" | wc -l)
-echo "After filtering out sites with too many low quality or missing variants, there are ${num_sites2} sites left"
+# Step 3: Filter out missing genotypes (GATK sets filtered genotypes to nocall *after* filtering for missing data)
+if [[ -f ${OUTPUT_DIR}/Intermediates/${OUT_PREFIX}_missing_filtered.vcf ]]; then
+	echo "Sites with too many low quality genotypes (set to missing by GATK in previous step) already filtered out, proceeding to step 4"
+else
+	echo "Removing sites with more than 0.2 low quality genotypes"
+	bcftools filter -e 'F_MISSING > 0.2' ${OUTPUT_DIR}/Intermediates/${OUT_PREFIX}_GenoFieldFiltered.vcf > ${OUTPUT_DIR}/Intermediates/${OUT_PREFIX}_missing_filtered.vcf
+	num_sites3=$(grep -v "#" "${OUTPUT_DIR}/Intermediates/${OUT_PREFIX}_missing_filtered.vcf" | wc -l)
+	echo "After filtering out sites with too many low quality or missing variants, there are ${num_sites3} sites left"
+fi
 
-### NOTE: It was discovered that the order of filtering by gatk was that it removed nocall-fraction genotypes *before* it set filtered genotypes to no-call,
-# necessitating an intermediate step here to filter out missing data - used bcftools (see bcftools_missingfilter.sh)
+# Step 4: Filtering out sites with too many heterozygous genotypes
+if [[ -f ${OUTPUT_DIR}/Intermediates/${OUT_PREFIX}_HETFiltered.vcf ]]; then
+	echo "Sites with more than ${het_prop} heterozygous genotypes already filtered out, proceeding to step 5"
+else
+	echo "Filtering out sites with more than ${het_prop} heterozygous genotypes"
+	bcftools filter -i 'COUNT(GT="het")/(N_SAMPLES-N_MISSING) < 0.2' ${OUTPUT_DIR}/Intermediates/${OUT_PREFIX}_missing_filtered.vcf -o ${OUTPUT_DIR}/Intermediates/${OUT_PREFIX}_HETFiltered.vcf
+	num_sites4=$(grep -v "#" "${OUTPUT_DIR}/Intermediates/${OUT_PREFIX}_HETFiltered.vcf" | wc -l)
+	echo "After filtering out sites with more than ${het_prop} heterozygous sites, there are ${num_sites4} sites remaining"
+fi
 
-# Step 3: Filtering out sites with too many heterozygous genotypes
-
-echo "Filtering out sites with more than ${het_prop} heterozygous genotypes"
-
-bcftools filter -i 'COUNT(GT="het")/(N_SAMPLES-N_MISSING) < 0.2' ${OUTPUT_DIR}/Intermediates/${OUT_PREFIX}_GenoFieldFiltered.vcf -o ${OUTPUT_DIR}/Intermediates/${OUT_PREFIX}_HETFiltered.vcf
-
-num_sites3=$(grep -v "#" "${OUTPUT_DIR}/Intermediates/${OUT_PREFIX}_HETFiltered.vcf" | wc -l)
-echo "After filtering out sites with more than ${het_prop} heterozygous sites, there are ${num_sites3} sites remaining"
-
-# Step 4: Filtering out sites with low QUAL values or high ExcessHet values
-
-echo "Removing sites with QUAL values below ${QUAL} and/or ExcessHet values above ${MaxExcessHet}"
-
+# Step 5: Filtering out sites with low QUAL values or high ExcessHet values
+if [[ -f ${OUTPUT_DIR}/${OUT_PREFIX}_FINALFilter.vcf.idx ]]; then
+	echo "Sites with QUAL values below ${QUAL} and/or ExcessHet values above ${MaxExcessHet} already filtered out, proceeding to step 6"
+else
+	echo "Removing sites with QUAL values below ${QUAL} and/or ExcessHet values above ${MaxExcessHet}"
 # first index file
-gatk --java-options "-Xmx2g" IndexFeatureFile \
--F ${OUTPUT_DIR}/Intermediates/${OUT_PREFIX}_HETFiltered.vcf
+	gatk --java-options "-Xmx2g" IndexFeatureFile \
+		-F ${OUTPUT_DIR}/Intermediates/${OUT_PREFIX}_HETFiltered.vcf
 
-gatk --java-options "-Xmx22g" SelectVariants \
--R ${GEN_FASTA} \
--V ${OUTPUT_DIR}/Intermediates/${OUT_PREFIX}_HETFiltered.vcf \
--O ${OUTPUT_DIR}/${OUT_PREFIX}_FINALFilter.vcf \
---selectExpressions "ExcessHet < ${MaxExcessHet}" \
---selectExpressions "QUAL > ${QUAL}" \
---exclude-non-variants true \
---tmp-dir ${TEMP_DIR}
+	gatk --java-options "-Xmx22g" SelectVariants \
+		-R ${GEN_FASTA} \
+		-V ${OUTPUT_DIR}/Intermediates/${OUT_PREFIX}_HETFiltered.vcf \
+		-O ${OUTPUT_DIR}/${OUT_PREFIX}_FINALFilter.vcf \
+		--selectExpressions "ExcessHet < ${MaxExcessHet}" \
+		--selectExpressions "QUAL > ${QUAL}" \
+		--exclude-non-variants true \
+		--tmp-dir ${TEMP_DIR}
 
-num_sites4=$(grep -v "#" "${OUTPUT_DIR}/${OUT_PREFIX}_FINALFilter.vcf" | wc -l)
-echo "After filtering for QUAL and ExcessHet annotations, there are ${num_sites4} sites remaining"
-
-
-# Step 5: Select only biallelic sites
-
-echo "Selecting biallelic sites"
-
-bcftools view -m2 -M2 -v snps --threads 4 ${OUTPUT_DIR}/${OUT_PREFIX}_FINALFilter.vcf --output-type v --output-file ${OUTPUT_DIR}/${OUT_PREFIX}_BIALLELIC.vcf
-
-num_sites5=$(grep -v "#" "${OUTPUT_DIR}/${OUT_PREFIX}_BIALLELIC.vcf" | wc -l)
-echo "After selecting only biallelic sites, there are ${num_sites5} variants"
+	num_sites5=$(grep -v "#" "${OUTPUT_DIR}/${OUT_PREFIX}_FINALFilter.vcf" | wc -l)
+	echo "After filtering for QUAL and ExcessHet annotations, there are ${num_sites5} sites remaining"
+fi
 
 
+# Step 6: Select only biallelic sites
+if [[ -f ${OUTPUT_DIR}/${OUT_PREFIX}_BIALLELIC.vcf ]]; then
+	echo "Biallelic sites already selected"
+else
+	echo "Selecting biallelic sites"
+	bcftools view -m2 -M2 -v snps --threads 4 ${OUTPUT_DIR}/${OUT_PREFIX}_FINALFilter.vcf --output-type v --output-file ${OUTPUT_DIR}/${OUT_PREFIX}_BIALLELIC.vcf
+	num_sites6=$(grep -v "#" "${OUTPUT_DIR}/${OUT_PREFIX}_BIALLELIC.vcf" | wc -l)
+	echo "After selecting only biallelic sites, there are ${num_sites6} variants"
+fi
