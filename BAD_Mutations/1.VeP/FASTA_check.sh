f6ee439813952aa5cfe748d71e6f5c869bbb3d88
Emily Dittmar <edittmar@uga.edu>
2021-03-16 18:46:34 -0400

    Create FASTA_check.sh

---
+++ BAD_Mutations/1.VeP/FASTA_check.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#SBATCH --job-name=CheckNRenameFASTAs
+#SBATCH --partition=batch
+
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=22G
+#SBATCH --time=36:00:00
+#SBATCH --export=None 
+#SBATCH --mail-user=dittmare@gmail.com
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH -o FastaCheck.out
+#SBATCH -e FastaCheck.err
+
+# checks that FASTA files all only have 1 sequence per file, # of characters divisible by 3 and renames + takes out mRNA prefix
+
+cd $FASTA_DIR
+
+for file in `ls -1`; do
+num_char=$(grep -v "^>" $file | grep -Eo '[[:alnum:]]' | wc -l)
+if (( $num_char % 3 != 0 )); then
+echo "ERROR: $file is not Divisible by 3"
+fi
+num_seq=$(grep "^>" $file | wc -l)
+if [[ $num_seq -ne 1 ]]; then
+echo "ERROR: $num sequences in $file"
+else
+sed -i 's/>mRNA:/>/' ${file}
+echo "Re-naming $file to ${file#mRNA:}sta"
+mv $file "${file#mRNA:}sta"
+fi
+done
