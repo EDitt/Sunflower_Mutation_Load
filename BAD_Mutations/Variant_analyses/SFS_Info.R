225b6836207e0c2ae7ee663bd11391d38df0c8df
Emily Dittmar <edittmar@uga.edu>
2022-01-12 15:54:17 -0500

    Split up so this script only does freq binning

--- BAD_Mutations/Variant_analyses/SFS_Info.R
+++ BAD_Mutations/Variant_analyses/SFS_Info.R
@@ -5,13 +5,11 @@
 options(warn=1)
 
 ### Command line arguments:
-# 1.) Directory containing the Chromosomes and positions of each class of variants (tab delimited)
-# 2.) File output from bcftools that has the number of alternate alleles and total count of alleles in called genotypes
-# 3.) Text file with chromosome, position, and ancestral allele
-# 4.) Output filename for MAF data
-# 5.) Output filename for derived frequency data
-# 6.) Output filename for RData file that contains entire table
-
+# 1.) Table containing frequency and annotation information for each variant
+# 2.) Max Frequency for histogram binning (e.g. 0.5 for MAF histogram)
+# 3.) Increment for histogram binning (e.g. 0.05)
+# 4.) Column to calculate frequency bins (e.g. "Derived_Freq")
+# 5.) Output filename for frequency data
 
 #########################
 ##### READ IN DATA ######
@@ -19,104 +17,50 @@ options(warn=1)
 
 args <- commandArgs(trailingOnly = TRUE)
 
-Directory <- args[1]
-SNP_freq_table <- args[2]
-Ancestral_file <- args[3]
-MAF_Output_File <- args[4]
-Derived_Output_File <- args[5]
-Frequency_RData_File <- args[6]
+SNP_table <- args[1]
+Max_freq <- args[2]
+Increment <- args[3]
+FrequencyColumn <- args[4]
+Output_File <- args[5]
 
 #########################
 ####### FUNCTIONS #######
 #########################
 
-# import tables with positions for various variant classes and edit variant class names for clarity
-ImportTxts <- function (DirPath) {
-  my_files <- list.files(path = DirPath, pattern = "*.txt", full.names = TRUE)
-  my_data <- lapply(my_files, read.table)
-  names(my_data) <- gsub("\\.txt$", "", my_files)
-  names(my_data) <- gsub(DirPath, "", names(my_data))
-  names(my_data) <- gsub("/", "", names(my_data))
-  names(my_data) <- gsub("Positions", "", names(my_data))
-  names(my_data) <- gsub("positions", "", names(my_data))
-  names(my_data) <- gsub("NoDups", "", names(my_data))
-  names(my_data) <- gsub("_", "", names(my_data))
-  for (i in seq_along(my_data)) {
-    name <- names(my_data[i])
-    colnames(my_data[[i]]) <- c("Chromosome", "Position")
-    my_data[[i]]["Variant_type"] <- name
-  }
-  return(my_data)
-}
-
-# calculate allele frequencies from bcftools output & merge with variant type information & ancestral allele information
-# outputs a list of dataframes split by variant type
-SNP_freq <- function (bcftools_file, ancestral_allele_file, annotation_table) {
-	data <- read.table(bcftools_file, sep = "\t", header=FALSE)
-	colnames(data) <- c("Chromosome", "Position", "Ref_allele", "Alt_allele",
-		"Num_Alt_alleles", "Num_alleles", "Alt_Freq")
-	data$Altfreq <- data$Num_Alt_alleles / data$Num_alleles
-	data$Reffreq <- (data$Num_alleles - data$Num_Alt_alleles) / data$Num_alleles
-	data$MAF <- ifelse(data$Reffreq < data$Altfreq, data$Reffreq, data$Altfreq)
-	Ancestral_table <- read.table(ancestral_allele_file, sep="\t", header=FALSE)
-	colnames(Ancestral_table) <- c("Chromosome", "Position", "Ancestral_Allele")
-	data_Anc <- merge(data, Ancestral_table, by=c("Chromosome", "Position"), all.x=TRUE)
-	data_Anc$Derived_Freq <- ifelse(data_Anc$Ancestral_Allele==data_Anc$Ref_allele,
-	data_Anc$Altfreq, ifelse(data_Anc$Ancestral_Allele==data_Anc$Alt_allele,
-		data_Anc$Reffreq, NA))
-	data_Anc <- with(data_Anc, data_Anc[order(Chromosome, Position),])
-	All_data <-  merge(data_Anc, annotation_table, by=c("Chromosome", "Position"))
-	All_dataList <- split(All_data, factor(All_data$Variant_type))
-  	return(All_dataList)
-}
 
 # get histogram bins
 Hist_bins <- function (dataset, hist_breaks, colName, Annotation) {
 	y <- hist(dataset[,colName], plot=FALSE, breaks=hist_breaks)
 	prop <- y$counts / sum(y$counts)
-	hist_df <- cbind.data.frame(hist_breaks[-1], prop, Annotation)
+	hist_df <- cbind.data.frame(hist_breaks[-1], prop, y$counts, Annotation)
 	return(hist_df)
 }
 
 
 #########################
+##### READ IN DATA ######
+#########################
+
+All_data <- read.table(SNP_table, sep = "\t", header=TRUE)
+
+# split by variant type
+All_dataList <- split(All_data, factor(All_data$Variant_type))
+
+# Frequency breaks
+breaks <- seq(from=0, to = Max_freq, by=Increment)
+
+
+###########################
 # APPLY FUNCTIONS TO DATA #
-#########################
-
-# assign each SNP to a variant class
-Position_lists <- ImportTxts(Directory)
-Positions_annotate <- do.call("rbind", Position_lists)
-
-# get allele frequency data from positions output and merge with variant type- output a list separated by variant type
-FrequencyInfo <- SNP_freq(SNP_freq_table, Ancestral_file, Positions_annotate)
-
-save(FrequencyInfo, file=Frequency_RData_File)
-
-#########################
-######## FOLDED #########
-#########################
-
-# histogram breaks for folded SFS
-MAF_breaks <- c(0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5)
+###########################
 
 # apply function across MAF data
-MAF_histList <- lapply(names(FrequencyInfo), function(x) {
-	Hist_bins(FrequencyInfo[[x]], MAF_breaks, "MAF", x)
+HistList <- lapply(names(All_dataList), function(x) {
+	Hist_bins(All_dataList[[x]], breaks, FrequencyColumn, x)
 	})
 
-MAF_histogram_data <- do.call("rbind", MAF_histList)
-colnames(MAF_histogram_data)[1] <- "breaks"
-write.table(MAF_histogram_data, MAF_Output_File, sep = "\t", quote=FALSE, row.names=FALSE)
+histogram_data <- do.call("rbind", HistList)
+colnames(histogram_data)[1] <- "breaks"
+colnames(histogram_data)[3] <- "count"
 
-#########################
-####### UNFOLDED ########
-#########################
-
-# apply function for derived allele data
-derived_breaks <- c(0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1)
-derived_histList <- lapply(names(FrequencyInfo), function(x) {
-	Hist_bins(FrequencyInfo[[x]], derived_breaks, "Derived_Freq", x)
-	})
-Der_histogram_data <- do.call("rbind", derived_histList)
-colnames(Der_histogram_data)[1] <- "breaks"
-write.table(Der_histogram_data, Derived_Output_File, sep = "\t", quote=FALSE, row.names=FALSE)
+write.table(histogram_data, Output_File, sep = "\t", quote=FALSE, row.names=FALSE)
