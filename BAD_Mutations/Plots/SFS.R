764a580cad29c5e9faf1852f7c823d09b0bf391f
Emily Dittmar <edittmar@uga.edu>
2022-06-16 15:50:48 -0400

    unfolded SFS graph

--- BAD_Mutations/Plots/SFS.R
+++ BAD_Mutations/Plots/SFS.R
@@ -41,4 +41,27 @@ ggsave("/Volumes/GoogleDrive/My Drive/Active Projects/DelMutation/Manuscript/Sun
 ##### VARIANT CLASSES - UNFOLDED ######
 #######################################
 
+unfolded_sfs <- read.table("/Volumes/GoogleDrive/My Drive/Active Projects/DelMutation/Results/SFS/DerivedFreq_Bins.txt",
+                         sep="\t", header = T)
 
+unfolded_sfs$Annotation <- factor(folded_sfs$Annotation, levels=c("NonCodingNodups", "SynonymousNodups",
+                                                                "Tolerated", "MissenseOther", "AllDel", 
+                                                                "StartStopLostGainednoDups", "SpliceAcceptorDonorNodups"))
+
+unfolded_sfs_subset <- subset(unfolded_sfs, Annotation %in% AnnotationsToKeep)
+
+p2 <- ggplot(unfolded_sfs_subset, aes(x=breaks, y=prop, fill=Annotation))
+#p2 <- ggplot(unfolded_sfs, aes(x=breaks, y=prop, fill=Annotation))
+p2 + geom_bar(stat="identity", position = position_dodge()) + theme_minimal() +
+  #scale_fill_brewer(palette = "Dark2") +
+  scale_fill_manual(values= c(Col_Synonymous, Col_Tolerated, Col_Deleterious),
+                    name = "Variant Class", labels = c("Synonymous", "Tolerated", "Deleterious")) +
+  ylab("Proportion") +
+  xlab("Frequency") +
+  #scale_x_continuous(breaks = c(0.05, 0.10, 0.15, 0.20, 0.25, 0.30, 0.35, 0.40, 0.45, 0.50),
+  #                   labels = c("[0,0.05)", "[0.05,0.10)", "[0.10,0.15)", "[0.15,0.20)", 
+  #                              "[0.20,0.25)", "[0.25,0.30)", "[0.30,0.35)", "[0.35,0.40)", "[0.40,0.45)", "[0.45,0.50)")) +
+  theme(panel.grid.minor = element_blank())
+
+ggsave("/Volumes/GoogleDrive/My Drive/Active Projects/DelMutation/Manuscript/Sunflower_MutationLoad_Manuscript/Sunflower_MutationLoad_v1/RawFigs/UnFoldedSFS.pdf",
+       width = 10, height = 5)
