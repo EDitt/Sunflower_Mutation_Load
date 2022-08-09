60f2e1fa4fdb0332834877991024042cd50a79ab
Emily Dittmar <edittmar@uga.edu>
2022-08-09 02:53:08 -0400

    need to load modules

--- BAD_Mutations/Variant_analyses/Scripts/ROH_SNPnums.sh
+++ BAD_Mutations/Variant_analyses/Scripts/ROH_SNPnums.sh
@@ -14,6 +14,10 @@
 #SBATCH --array=0-287
 
 
+module load R/4.0.0-foss-2019b
+
+module load BCFtools/1.13-GCC-8.3.0
+
 # calculates number of SNPs in a specific genomic region (i.e. ROH) for a specific genotype, across variant classes
 
 # variables that need to be defined
